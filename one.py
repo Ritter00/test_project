86d24859b74172658befb5426c32351218ac5256
Ritter00 <79079398+Ritter00@users.noreply.github.com>
2021-07-06 11:03:10 +0300

    Delete one.py

--- one.py
+++
@@ -1,6 +0,0 @@
-Python 3.8.8 (tags/v3.8.8:024d805, Feb 19 2021, 13:08:11) [MSC v.1928 32 bit (Intel)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> from math import sqrt
->>> print(sqrt(256)-100)
--84.0
->>> 
